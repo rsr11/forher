d181bd4168d7db5ad3cef7ce159d044d43fe3bcb
Rajeshwar <rsr45411@gmail.com>
2026-03-01 18:43:59 +0530

    change a ved

--- src/components/TheEnd.jsx
+++ src/components/TheEnd.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const TheEnd = () => {
   return (
     <section className='bg-black'>
-         <h1 className='text-9xl pt-[200px] pb-20 text-center text-pinky' >GOOD NIGHT 🧸</h1>
+        <div className='flex items-center bg-re-100 justify-center gap-2'>
+          {/* <img src="/images/propicc.png" className='w-30 mt-20 bg-ed-500 object-contain' alt="" />  */}
+         <h1 className='text-9xl pt-[200px] pb-20 texer text-pinky' >GOOD NIGHT 🧸 </h1>
+          </div>
     </section>
   )
 }
